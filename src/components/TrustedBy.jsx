690294deb9f8779bdcec5f266daa2ef092c2e973
ifechukwu-udechukwu <peterifechukwuudechukwu@gmail.com>
2025-09-27 10:26:54 +0100

    added leading companies logo

---
+++ src/components/TrustedBy.jsx
@@ -0,0 +1,21 @@
+
+const TrustedBy = () => {
+  return (
+    <div className="flex flex-col items-center px-4 sm:px-12 lg:px-24 xl:px-40 gap-10 text-gray-700 dark:text-white/80">
+        <h3 className="font-semibold">Trusted by Leading Companies</h3>
+
+        <div className="flex items-center justify-center flex-wrap gap-10 m-4">
+            <img src="/Starburst.png" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/mercedes.png" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/Ferrari.jpg" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/Apple.jpg" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/Youtube.png" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/Amazon.png" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/BMW.jpg" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+            <img src="/Ford.png" alt="logo" className="max-h-8 sm:max-h-9 dark:drop-shadow-xl"/>
+        </div>
+    </div>
+  )
+}
+
+export default TrustedBy
